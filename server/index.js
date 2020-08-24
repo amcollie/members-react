b1e97011384d50a82ab9b8d3a3215c4c057ab4f8
Alexandros Collie <amcollie@gmail.com>
2020-08-24 02:05:08 -0400

    adjustments for hroku deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const ENV = process.env.NODE_DEV;
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
@@ -9,6 +11,12 @@ app.use(express.json());
 
 app.use('/members', require('./api/members'));
 
+if (ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use((req, res) => {
+        res.sendfile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
 app.get('/', (req, res) => {
     res.status(200).json('Welcome to Members API');
 });
